0f25f78115644e940d5d44e88c309440ec86f279
brijenshah <brijen.shah@live.com>
2016-01-06 21:45:05 -0800

    Added TrieNode

---
+++ Solutions/Definition/TrieNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Solutions.Definition
+{
+    public class TrieNode
+    {
+        public char Data;
+        public Dictionary<char, TrieNode> Children = new Dictionary<char, TrieNode>();
+        public bool IsLeaf;
+        // Initialize your data structure here.
+        public TrieNode()
+        {
+        }
+
+        public TrieNode(char c)
+        {
+            Data = c;
+        }
+    }
+}
+
